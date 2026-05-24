7c84d2c6c6bbfb932c6b29a60687479141aaaf92
khrore <khrore1@gmail.com>
2026-05-24 19:43:46 +0300

    not nix store in env

--- dotfiles/common/.orbstack/shell/init.bash
+++ dotfiles/common/.orbstack/shell/init.bash
@@ -1,3 +1,3 @@
 export PATH="$PATH":/Users/khrore/.orbstack/bin
 
-[[ -n "$BASH_COMPLETION" ]] && (. /nix/store/v342n4hx8mvy65hiais5ldiany6qz01v-orbstack-2.1.3-20115/Applications/OrbStack.app/Contents/MacOS/../Resources/completions/bash/*.bash 2>/dev/null || :)
+[[ -n "$BASH_COMPLETION" ]] && (. /Applications/OrbStack.app/Contents/MacOS/../Resources/completions/bash/*.bash 2>/dev/null || :)
--- dotfiles/common/.orbstack/shell/init.zsh
+++ dotfiles/common/.orbstack/shell/init.zsh
@@ -1,3 +1,3 @@
 export PATH="$PATH":/Users/khrore/.orbstack/bin
 
-fpath+=/nix/store/v342n4hx8mvy65hiais5ldiany6qz01v-orbstack-2.1.3-20115/Applications/OrbStack.app/Contents/MacOS/../Resources/completions/zsh
+fpath+=/Applications/OrbStack.app/Contents/MacOS/../Resources/completions/zsh
