ab03bb353c9f466b8b3424e4039d6cf5f3b1bf2c
khrore <khrore1@gmail.com>
2026-09-18 11:48:54 +0300

    settings and shell

--- dotfiles/common/.config/zsh/env.zsh
+++ dotfiles/common/.config/zsh/env.zsh
@@ -11,7 +11,16 @@ export HISTSIZE="10000"
 export SAVEHIST="10000"
 
 export NIXOS_CONFIG="$HOME/my-nix"
-export NH_OS_FLAKE="$HOME/my-nix"
+
+export NH_FLAKE="$HOME/my-nix"
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  alias sw='nh darwin switch'
+  alias upd='nh darwin switch --update'
+else
+  alias sw='nh os switch'
+  alias upd='nh os switch --update'
+fi
 
 export NIX_BUILD_SHELL="bash"
 
