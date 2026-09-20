ac68044a5ff9572916465447dd71dfa7f86e34a7
khrore <khrore1@gmail.com>
2026-09-20 20:14:16 +0300

    lazyvim fixed

--- dotfiles/common/.config/nvim/lua/plugins/yazi.lua
+++ dotfiles/common/.config/nvim/lua/plugins/yazi.lua
@@ -1,16 +1,15 @@
 ---@type LazySpec
 return {
 	"mikavilpas/yazi.nvim",
+	version = "*", -- use the latest stable version
 	event = "VeryLazy",
 	dependencies = {
-		-- check the installation instructions at
-		-- https://github.com/folke/snacks.nvim
-		"folke/snacks.nvim",
+		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
 	keys = {
 		-- 👇 in this section, choose your own keymappings!
 		{
-			"<leader>we",
+			"<leader>-",
 			mode = { "n", "v" },
 			"<cmd>Yazi<cr>",
 			desc = "Open yazi at the current file",
@@ -37,8 +36,9 @@ return {
 	},
 	-- 👇 if you use `open_for_directories=true`, this is recommended
 	init = function()
+		-- mark netrw as loaded so it's not loaded at all.
+		--
 		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-		-- vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 	end,
 }
